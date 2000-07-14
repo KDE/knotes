c2f38264ba3b442f76cbaf86b9c7098d13f649cb
Wynn Wilkes <wynnw@wilkes.ws>
2000-07-14 20:07:41 +0000

    removed WABA's net entry for removing window borders... KWin is respecting the Qt flags again it seems, it works without this now
    svn path=/trunk/kdeutils/knotes/; revision=56906

--- knote.cpp
+++ knote.cpp
@@ -23,19 +23,15 @@ KNote::KNote( KConfig* config, QWidget* parent, const char* name )
 {
 	kdDebug() << "start Knote::Knote" << endl;
 
-	//WABA: Get rid of decorations. I can hardly imagine that 
-	//this is "The Right Way" of doing that.
-	KWin::setType( winId(), NET::Toolbar);
-	
 	m_config = config;
 	m_notedir = new QDir( KGlobal::dirs()->saveLocation( "appdata", "notes/" ) );
 	setFrameStyle( NoFrame );
-	
+
 	//create the note header- button and label...
 	m_button = new KNoteButton( this );
 	m_button->setPixmap( BarIcon( "knotesclose" ) );
 	connect( m_button, SIGNAL( clicked() ), this, SLOT( slotClose() ) );
-	
+
 	m_label = new QLabel( this );
 	m_label->setAlignment( AlignHCenter );
 	m_label->setText( m_config->entryMap("Data")["name"] );
@@ -52,7 +48,7 @@ KNote::KNote( KConfig* config, QWidget* parent, const char* name )
    		QString absfile = m_notedir->absFilePath( datafile );
    		m_editor->readFile( absfile );
    	}
-   	
+
     //apply configuration settings
     slotApplyConfig();
 
@@ -86,7 +82,7 @@ KNote::~KNote()
 		QString absdatafile = m_notedir->absFilePath( datafile );
 		m_editor->dumpToFile( absdatafile );
 	}
-	
+
 	if( m_config )
 		m_config->sync();
 }
@@ -96,7 +92,7 @@ KNote::~KNote()
 void KNote::resizeEvent( QResizeEvent* qre )
 {
 	QFrame::resizeEvent( qre );
-	
+
 	m_button->setGeometry( width() - 15, 0, 15, 15 );
 	m_label->setGeometry( 0, 0, width() - 15, 15 );
 	m_editor->setGeometry( 0, 15, width(), height() - 15 );
@@ -108,38 +104,38 @@ void KNote::slotApplyConfig()
 	int width  = (m_config->entryMap( "Display" )["width"]).toInt();
 	int height = (m_config->entryMap( "Display" )["height"]).toInt();
 	resize( width, height );
-	
+
 	//create a pallete...
 	QColor bg = KNoteConfigDlg::getBGColor( *m_config );
 	QColor fg = KNoteConfigDlg::getFGColor( *m_config );
-	
+
 	QPalette newpalette = palette();
 	newpalette.setColor( QColorGroup::Background, bg );
 	newpalette.setColor( QColorGroup::Base,       bg );
 	newpalette.setColor( QColorGroup::Foreground, fg );
 	newpalette.setColor( QColorGroup::Text,       fg );
 	setPalette( newpalette );
-	
-	//set darker values for the label and button...	
+
+	//set darker values for the label and button...
 	m_label->setBackgroundColor( bg.dark( 120 ) );
 	m_button->setBackgroundColor( bg.dark( 120 ) );
-	
-	//do the Editor group: tabsize, autoindent, font, fontsize, fontstyle	
+
+	//do the Editor group: tabsize, autoindent, font, fontsize, fontstyle
 	QString str_font = m_config->entryMap("Editor")["font"];
 	int font_size    =(m_config->entryMap("Editor")["fontsize"]).toInt();
 	int fontstyle    =(m_config->entryMap("Editor")["fontstyle"]).toInt();
 	m_editor->setFont( QFont( str_font, font_size ) );
-		
+
 	int tab_size = (m_config->entryMap("Editor")["tabsize"]).toInt();
 	m_editor->setDefaultTabStop( tab_size );
-	
+
 	QString indent = m_config->entryMap("Editor")["autoindent"];
 	if( indent == "yes" )
 		 m_editor->setAutoIndentMode( true );
 	else m_editor->setAutoIndentMode( false );
 
 	//do Actions Group, mail, print, date
-	
+
 	//do the Data Group- name, data
 	QString notename = m_config->entryMap("Data")["name"];
 	m_label->setText( notename );
@@ -205,7 +201,7 @@ bool KNote::eventFilter( QObject* o, QEvent* ev )
 			m_menu->popup( QCursor::pos() );
 			return true;
 		}
-			
+
 		return m_label->eventFilter( o, ev );
 	}
 
@@ -218,11 +214,11 @@ void KNote::slotMail( int id )
 	QString data_name = m_config->entryMap("Data")["name"] + "_data";
 	data_name = m_notedir->absFilePath( data_name );
 	m_editor->dumpToFile( data_name );
-	
+
 	QString fname = "\"";
 	fname += data_name;
 	fname += "\"";
-	
+
 	//KMail doesn't really respect the --msg option it seems??
 	KProcess mail;
 	mail << "kmail" << "--composer" << "--msg" << fname;
@@ -233,15 +229,15 @@ void KNote::slotMail( int id )
 
 void KNote::slotRename( int id )
 {
-	QString newname;	
+	QString newname;
 	QString oldname = m_config->entryMap("Data")["name"];
 	QString oldname_data = oldname + "_data";
-	
+
 	while( true )
 	{
 		//pop up dialog to get the new name
     	newname = KLineEditDlg::getText( i18n("Please enter the new name"), QString::null, NULL, this );
-    	
+
 		//check the name to make sure that it's not already used
 		if( m_notedir->exists( newname ) )
 		{
@@ -254,7 +250,7 @@ void KNote::slotRename( int id )
 				i18n("A name must have at least one character") );
 		else break;
 	}
-	
+
 	//close config, copy old file to new name
 	m_config->setGroup( "Data" );
 	m_config->writeEntry( "name", newname );
@@ -265,14 +261,14 @@ void KNote::slotRename( int id )
 		kdDebug() << "rename failed" << endl;
 	if( !m_notedir->rename( "."+oldname+"_data", "."+newname+"_data" ) )
 		kdDebug() << "rename of data file failed" << endl;
-	
+
 	//open new config
 	QString newconfig = m_notedir->absFilePath( newname );
 	m_config = new KConfig( newconfig );
 
 	//redo the label text
 	m_label->setText( newname );
-		
+
 	//emit signal with oldname and newname
 	emit sigRenamed( oldname, newname );
 }
@@ -282,16 +278,16 @@ void KNote::slotInsDate( int id )
 	int line, column;
 	m_editor->getCursorPosition( &line, &column );
 	m_editor->insertAt( KGlobal::locale()->formatDateTime(QDateTime::currentDateTime()),
-	                    line, column );	
+	                    line, column );
 }
 
 void KNote::slotConfig ( int id )
 {
 	//launch config dialog...
-	KNoteConfigDlg* localConfig = new KNoteConfigDlg( m_config, i18n("Local Settings") );	
+	KNoteConfigDlg* localConfig = new KNoteConfigDlg( m_config, i18n("Local Settings") );
 	connect( localConfig, SIGNAL( updateConfig() ),
 	         this, SLOT( slotApplyConfig() ) );
-	
+
 	//launch preferences dialog...
 	localConfig->show();
 }
@@ -304,15 +300,15 @@ void KNote::slotClose()
 	m_editor->dumpToFile( absname );
 
 	m_config->sync();
-	
+
 	close();
-	
+
 	emit sigClosed( m_config->entryMap("Data")["name"] );
 }
 
 void KNote::slotKill( int id )
 {
-    //delete the config file...	
+    //delete the config file...
 	QString conf_name = m_config->entryMap("Data")["name"];
     m_notedir->remove( conf_name );
     m_notedir->remove( "." + conf_name + "_data" );
@@ -324,7 +320,7 @@ void KNote::slotKill( int id )
    	emit sigClosed( m_config->entryMap("Data")["name"] );
    	delete m_config;
    	m_config = NULL;
-   	
+
 	close();
  }
 
