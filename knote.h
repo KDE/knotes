ed8ff7cf67a3dc59c5d1b8ad7dac8a7abb0eb862
Wynn Wilkes <wynnw@wilkes.ws>
2000-09-14 16:18:44 +0000

    see commit for knote.cpp
    svn path=/trunk/kdeutils/knotes/; revision=63873

--- knote.h
+++ knote.h
@@ -6,7 +6,7 @@
 #include "knoteedit.h"
 #include "knotebutton.h"
 
-#include <kconfig.h>
+#include <ksimpleconfig.h>
 #include <kpopupmenu.h>
 #include <qframe.h>
 #include <qlabel.h>
@@ -16,49 +16,58 @@
 class KNote : public QFrame  {
    Q_OBJECT
 public: 
-	KNote( KConfig* config, QWidget* parent=0, const char* name=0 );
-	~KNote();
-	
-	void save();
-	QString getName()
-	{
-	    if( m_label )
-	         return m_label->text();
-	    else return QString::null;
-	}
+    KNote( KSimpleConfig* config, QWidget* parent=0, const char* name=0 );
+    ~KNote();
+
+    void save();
+    QString getName();
+    uint currDesktop();
+    void setOnDesktop( uint id );
+
 
 public slots:
-	//menu slots
-	void slotMail   ( int id=0 );
-	void slotPrint  ( int id=0 );
-	void slotRename ( int id=0 );
-	void slotInsDate( int id=0 );
-	void slotConfig ( int id=0 );
-	void slotKill   ( int id=0 );
-	void slotClose  ();
-	void slotApplyConfig();
-	
+    //menu slots
+    void slotMail   ( int );
+    void slotPrint  ( int );
+    void slotRename ( int );
+    void slotInsDate( int );
+    void slotConfig ( int );
+    void slotKill   ( int );
+    void slotClose  ();
+
+    void slotApplyConfig();
+    void slotToDesktop( int id );
+    void slotPrepareDesktopMenu();
+    void slotAlwaysOnTop( int );
+
+    virtual void setFocus();
+    virtual void show();
+
 signals:
-	void sigClosed( QString& );
-	void sigRenamed( QString&, QString& );
-	void sigNewNote( int );
-	
+    void sigKilled( QString );
+    void sigRenamed( QString&, QString& );
+    void sigNewNote( int );
+
 protected:
-	void resizeEvent( QResizeEvent* );
-	bool eventFilter( QObject* o, QEvent* e );
-			
-	KNoteEdit*   m_editor;
-    QString      m_title;
-    KConfig*     m_config;
-    KNoteButton* m_button;
-    QLabel*      m_label;
-    KPopupMenu*  m_menu;
-    int          m_headerHeight;
-	
-    bool         m_dragging;
-    QPoint       m_pointerOffset;
-    int          m_id;
-    QDir*        m_notedir;
+    void resizeEvent( QResizeEvent* );
+    bool eventFilter( QObject* o, QEvent* e );
+
+    KNoteEdit*     m_editor;
+    QString        m_title;
+    KSimpleConfig* m_config;
+    KNoteButton*   m_button;
+    QLabel*        m_label;
+    KPopupMenu*    m_menu;
+    KPopupMenu*    m_desktop_menu;
+    int            m_headerHeight;
+
+    bool           m_dragging;
+    QPoint         m_pointerOffset;
+    int            m_id;
+    QDir*          m_notedir;
+
+    int            m_idAlwaysOnTop;
+
 };
 
 
