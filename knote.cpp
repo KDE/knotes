f75c88bf3c70f83fe7b9b1effcf2c4a47f53211e
Stephan Kulow <coolo@kde.org>
2002-07-04 08:51:19 +0000

    That's the penalty of using private interfaces -        textDoc->setDefaultFont( font );        // only needed for the pointsize +#if QT_VERSION > 304 +        textDoc->setDefaultFormat( font, Qt::black );        // only needed for the pointsize +#else +       textDoc->setDefaultFont( font );        // only needed for the pointsize +#endif
    svn path=/trunk/kdepim/; revision=164704

--- knote.cpp
+++ knote.cpp
@@ -497,7 +497,11 @@ void KNote::slotPrint() const
 
         QTextDocument* textDoc = new QTextDocument( 0 );
         textDoc->setFormatter( new QTextFormatterBreakWords );
-        textDoc->setDefaultFont( font );        // only needed for the pointsize
+#if QT_VERSION > 304
+        textDoc->setDefaultFormat( font, Qt::black );        // only needed for the pointsize
+#else
+	textDoc->setDefaultFont( font );        // only needed for the pointsize
+#endif
         textDoc->setUnderlineLinks( true );
         textDoc->setStyleSheet( m_editor->styleSheet() );
         textDoc->setMimeSourceFactory( m_editor->mimeSourceFactory() );
