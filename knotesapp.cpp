b080df92a7df19cc93de5daa4ce38a58e9fa9af4
Stephan Binner <binner@kde.org>
2003-07-25 19:31:22 +0000

    CVS_SILENT This sneaked in. :-(
    svn path=/trunk/kdepim/; revision=239162

--- knotesapp.cpp
+++ knotesapp.cpp
@@ -486,8 +486,8 @@ void KNotesApp::saveNotes()
     // (might just destroy the file that's already there)
 
     if ( KIO::NetAccess::exists( KURL( file ) ) && !KIO::NetAccess::file_copy( KURL( file ), KURL( backup ), -1, true) )
-        KMessageBox::error(0, i18n("<qt>Can't save the notes backup to <b>%1</b>! Harddisk full?%2</qt>")
-                                  .arg( backup ).arg(file) );
+        KMessageBox::error(0, i18n("<qt>Can't save the notes backup to <b>%1</b>! Harddisk full?</qt>")
+                                  .arg( backup ) );
     else if ( !m_calendar.save( file, new ICalFormat() ) )
         KMessageBox::error(0, i18n("<qt>Can't save the notes to <b>%1</b>! Harddisk full? \
                                     There should be a backup in <b>%2</b> though.</qt>")
