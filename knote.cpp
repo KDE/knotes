441eb964de4eb7c859e07f6c969fce5a12b71e44
Eduardo Robles Elvira <edulix@gmail.com>
2008-03-29 12:05:46 +0000

    Remove checks for Qt 4.4 as its use is mandatory in trunk
    svn path=/trunk/KDE/kdepim/knode/; revision=791398

--- knote.cpp
+++ knote.cpp
@@ -680,11 +680,7 @@ void KNote::createActions()
 
   actionCollection()->addAssociatedWidget( this );
   foreach (QAction* action, actionCollection()->actions())
-#if QT_VERSION < KDE_MAKE_VERSION(4,4,0)
-    action->setShortcutContext(Qt::WidgetShortcut); // remove after Qt4.4 becomes mandatory
-#else
     action->setShortcutContext(Qt::WidgetWithChildrenShortcut);
-#endif
 }
 
 void KNote::createNoteHeader()
