50b9811b94b91f0a9d95fc2e3011fedccb55abdc
Allen Winter <winter@kde.org>
2009-03-15 14:17:24 +0000

    backport SVN commit 939033 by mlaurent:
    SVN_SILENT: code style
    
    svn path=/branches/KDE/4.2/kdepim/; revision=939697

--- knoteedit.cpp
+++ knoteedit.cpp
@@ -420,7 +420,9 @@ void KNoteEdit::dropEvent( QDropEvent *e )
       }
       insertPlainText( ( *it ).prettyUrl() );
     }
-  } else { KTextEdit::dropEvent( e ); }
+  } else {
+      KTextEdit::dropEvent( e );
+  }
 }
 
 void KNoteEdit::keyPressEvent( QKeyEvent *e )
