3fec26cc1c15cae9a0785b627a3fc425316586a5
Laurent Montel <montel@kde.org>
2009-03-16 16:27:27 +0000

    Backport: fix crash when we 'save as'
    svn path=/branches/KDE/4.2/kdepim/; revision=940118

--- knote.cpp
+++ knote.cpp
@@ -452,7 +452,7 @@ void KNote::slotSaveAs()
     convert = new QCheckBox( 0 );
     convert->setText( i18n( "Save note as plain text" ) );
   }
-
+  m_blockEmitDataChanged = true;
   KUrl url;
   KFileDialog dlg( url, QString(), this, convert );
   dlg.setOperationMode( KFileDialog::Saving );
@@ -461,6 +461,7 @@ void KNote::slotSaveAs()
 
   QString fileName = dlg.selectedFile();
   if ( fileName.isEmpty() ) {
+      m_blockEmitDataChanged = false;
     return;
   }
 
@@ -471,6 +472,7 @@ void KNote::slotSaveAs()
           i18n( "<qt>A file named <b>%1</b> already exists.<br />"
                 "Are you sure you want to overwrite it?</qt>",
                 QFileInfo( file ).fileName() ) ) != KMessageBox::Continue ) {
+      m_blockEmitDataChanged = false;
     return;
   }
 
@@ -482,6 +484,7 @@ void KNote::slotSaveAs()
       stream << m_editor->toPlainText();
     }
   }
+  m_blockEmitDataChanged = false;
 }
 
 void KNote::slotPopupActionToDesktop( int id )
