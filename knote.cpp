4ebb43b725d8920892d0947a8b5b0d7f87e86b67
Thomas McGuire <mcguire@kde.org>
2009-03-18 13:55:46 +0000

    Merged revisions 940118 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r940118 | mlaurent | 2009-03-16 17:27:27 +0100 (Mon, 16 Mar 2009) | 2 lines
      
      Backport: fix crash when we 'save as'
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=940923

--- knote.cpp
+++ knote.cpp
@@ -451,7 +451,7 @@ void KNote::slotSaveAs()
     convert = new QCheckBox( 0 );
     convert->setText( i18n( "Save note as plain text" ) );
   }
-
+  m_blockEmitDataChanged = true;
   KUrl url;
   KFileDialog dlg( url, QString(), this, convert );
   dlg.setOperationMode( KFileDialog::Saving );
@@ -460,6 +460,7 @@ void KNote::slotSaveAs()
 
   QString fileName = dlg.selectedFile();
   if ( fileName.isEmpty() ) {
+      m_blockEmitDataChanged = false;
     return;
   }
 
@@ -470,6 +471,7 @@ void KNote::slotSaveAs()
           i18n( "<qt>A file named <b>%1</b> already exists.<br />"
                 "Are you sure you want to overwrite it?</qt>",
                 QFileInfo( file ).fileName() ) ) != KMessageBox::Continue ) {
+      m_blockEmitDataChanged = false;
     return;
   }
 
@@ -481,6 +483,7 @@ void KNote::slotSaveAs()
       stream << m_editor->toPlainText();
     }
   }
+  m_blockEmitDataChanged = false;
 }
 
 void KNote::slotPopupActionToDesktop( int id )
