2d2e2cd6d9faf3dafedd420643024cd8b5987be1
Montel Laurent <montel@kde.org>
2013-10-31 12:54:08 +0100

    rc files was moved to data directory

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC *.rc settings/*.kcfg >> rc.cpp
+$EXTRACTRC data/*.rc settings/*.kcfg >> rc.cpp
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp'` -o $podir/knotes.pot
 rm -f rc.cpp
