99cab6b3f826cd980dc41963e148dccf9bca9d23
Montel Laurent <montel@kde.org>
2013-11-04 09:01:25 +0100

    Fix extract messages

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC data/*.rc settings/*.kcfg >> rc.cpp
+$EXTRACTRC data/*.rc settings/*.kcfg settings/*.kcfg.cmake >> rc.cpp
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp'` -o $podir/knotes.pot
 rm -f rc.cpp
