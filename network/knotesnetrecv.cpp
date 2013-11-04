36d5cadeddb242065dd687d41670019ed3f3eadd
Montel Laurent <montel@kde.org>
2013-11-04 08:24:41 +0100

    const'ify

--- network/knotesnetrecv.cpp
+++ network/knotesnetrecv.cpp
@@ -126,8 +126,8 @@ void KNotesNetworkReceiver::slotConnectionClosed()
         QString noteText = QString( codec->toUnicode( *m_buffer ) ).trimmed();
 
         // First line is the note title or, in case of ATnotes, the id
-        int pos = noteText.indexOf( QRegExp( QLatin1String("[\r\n]") ) );
-        QString noteTitle = noteText.left( pos ).trimmed() + m_titleAddon;
+        const int pos = noteText.indexOf( QRegExp( QLatin1String("[\r\n]") ) );
+        const QString noteTitle = noteText.left( pos ).trimmed() + m_titleAddon;
 
         noteText = noteText.mid( pos ).trimmed();
 
