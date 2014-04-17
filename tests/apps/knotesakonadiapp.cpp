196b9cfe6d596defb7765f083c041caea9e4da83
Montel Laurent <montel@kde.org>
2014-04-17 15:38:38 +0200

    Fix Bug 333534 - Creating aknotes plasmoid crashes kontact
    FIXED-IN: 4.13.1
    BUG: 333534

--- tests/apps/knotesakonadiapp.cpp
+++ tests/apps/knotesakonadiapp.cpp
@@ -79,14 +79,16 @@ void KNotesAkonadiApp::slotItemChanged(const Akonadi::Item &item, const QSet<QBy
         }
         if (set.contains("PLD:RFC822")) {
             KMime::Message::Ptr noteMessage = item.payload<KMime::Message::Ptr>();
-            const KMime::Headers::Subject * const subject = noteMessage ? noteMessage->subject(false) : 0;
-            note->title()->setText(subject ? subject->asUnicodeString() : QString());
-            if ( noteMessage->contentType()->isHTMLText() ) {
-                note->editor()->setAcceptRichText(true);
-                note->editor()->setHtml(noteMessage->mainBodyPart()->decodedText());
-            } else {
-                note->editor()->setAcceptRichText(false);
-                note->editor()->setPlainText(noteMessage->mainBodyPart()->decodedText());
+            if (noteMessage) {
+                const KMime::Headers::Subject * const subject = noteMessage->subject(false);
+                note->title()->setText(subject ? subject->asUnicodeString() : QString());
+                if ( noteMessage->contentType()->isHTMLText() ) {
+                    note->editor()->setAcceptRichText(true);
+                    note->editor()->setHtml(noteMessage->mainBodyPart()->decodedText());
+                } else {
+                    note->editor()->setAcceptRichText(false);
+                    note->editor()->setPlainText(noteMessage->mainBodyPart()->decodedText());
+                }
             }
         }
         if (set.contains("ATR:NoteDisplayAttribute")) {
