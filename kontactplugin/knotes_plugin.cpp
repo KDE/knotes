e1c29e95b8e02a527b12d4407425a8034eda860a
Montel Laurent <montel@kde.org>
2015-07-07 16:21:18 +0200

    Use QStringLiteral

--- kontactplugin/knotes_plugin.cpp
+++ kontactplugin/knotes_plugin.cpp
@@ -160,7 +160,7 @@ void KNotesPlugin::processDropEvent(QDropEvent *event)
         }
         event->accept();
         static_cast<KNotesPart *>(part())->newNote(
-            i18nc("@item", "Meeting"), attendees.join(QLatin1String(", ")));
+            i18nc("@item", "Meeting"), attendees.join(QStringLiteral(", ")));
         return;
     }
 
