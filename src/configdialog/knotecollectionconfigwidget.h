dba2ae962edcf102cbf421a43ad38bca74ecca1b
Montel Laurent <montel@kde.org>
2016-01-27 21:37:56 +0100

    Fix copyright

--- src/configdialog/knotecollectionconfigwidget.h
+++ src/configdialog/knotecollectionconfigwidget.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2013-2015 Laurent Montel <montel@kde.org>
+    Copyright (C) 2013-2016 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
