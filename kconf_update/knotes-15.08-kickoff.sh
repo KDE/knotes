c9b89df5a392a84570b8fa238dd0802a8f77bb0b
Montel Laurent <montel@kde.org>
2016-02-22 16:43:26 +0100

    Test if file exists before to execute sed

--- kconf_update/knotes-15.08-kickoff.sh
+++ kconf_update/knotes-15.08-kickoff.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sed -i "s/\/knotes.desktop/\/org.kde.knotes.desktop/" `kf5-config --path config --locate kickoffrc`
+kickoffrcname=`kf5-config --path config --locate kickoffrc`
+if [ -f "$kickoffrcname" ]; then
+   sed -i "s/\/knotes.desktop/\/org.kde.knotes.desktop/" $kickoffrcname
+fi
