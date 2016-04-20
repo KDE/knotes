be5e63cd937bb3edcfa6913db156edb67d0b2982
Montel Laurent <montel@kde.org>
2016-04-20 08:08:24 +0200

    Fix includes

--- src/apps/knotesapp.cpp
+++ src/apps/knotesapp.cpp
@@ -18,7 +18,7 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 *******************************************************************/
 
-#include "config-kdepim.h"
+#include "config-knotes.h"
 #include "configdialog/knoteconfigdialog.h"
 
 #include "dialog/knoteselectednotesdialog.h"
