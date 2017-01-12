ddead8e3d78e667c0cbbe672f30cadd528bc5693
Montel Laurent <montel@kde.org>
2017-01-12 23:43:33 +0100

    Fix copyright

--- notesagent/notesagentsettingsdialog.cpp
+++ notesagent/notesagentsettingsdialog.cpp
@@ -72,7 +72,7 @@ NotesAgentSettingsDialog::NotesAgentSettingsDialog(QWidget *parent)
                                QStringLiteral(KNOTES_VERSION),
                                i18n("Notes Agent."),
                                KAboutLicense::GPL_V2,
-                               i18n("Copyright (C) 2013-2016 Laurent Montel"));
+                               i18n("Copyright (C) 2013-2017 Laurent Montel"));
 
     aboutData.addAuthor(i18n("Laurent Montel"),
                         i18n("Maintainer"), QStringLiteral("montel@kde.org"));
