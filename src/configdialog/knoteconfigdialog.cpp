d7f93680b7b26efe672304f310940465fb573ec4
Martin Koller <kollix@aon.at>
2018-01-23 08:25:22 +0100

    make it compile with clang
    Differential Revision:  https://phabricator.kde.org/D10029

--- src/configdialog/knoteconfigdialog.cpp
+++ src/configdialog/knoteconfigdialog.cpp
@@ -210,7 +210,7 @@ KNoteMiscConfig::KNoteMiscConfig(QWidget *parent)
     howItWorks->setContextMenuPolicy(Qt::NoContextMenu);
     lay->addStretch();
     load();
-    connect(mDefaultTitle, &QLineEdit::textChanged, this, QOverload<>::of(&KCModule::changed));
+    connect(mDefaultTitle, &QLineEdit::textChanged, this, QOverload<>::of(&KNoteMiscConfig::changed));
 }
 
 void KNoteMiscConfig::load()
@@ -317,7 +317,7 @@ KNoteCollectionConfig::KNoteCollectionConfig(QWidget *parent)
     QHBoxLayout *lay = new QHBoxLayout(this);
     mCollectionConfigWidget = new KNoteCollectionConfigWidget;
     lay->addWidget(mCollectionConfigWidget);
-    connect(mCollectionConfigWidget, &KNoteCollectionConfigWidget::emitChanged, this, QOverload<>::of(&KCModule::changed));
+    connect(mCollectionConfigWidget, &KNoteCollectionConfigWidget::emitChanged, this, QOverload<>::of(&KNoteCollectionConfig::changed));
     load();
 }
 
