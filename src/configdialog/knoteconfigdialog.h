7be8013bed25c6a55ad1641da3c1ab35ba5b289d
Laurent Montel <laurent.montel@kdab.com>
2021-01-08 08:10:51 +0100

    Fix Bug 431126 - KNotes cannot be configured using the GUI
    BUG: 431126
    (cherry picked from commit e9f67828c72c8dc2f2802d4736e85e2ccee6b32d)

--- src/configdialog/knoteconfigdialog.h
+++ src/configdialog/knoteconfigdialog.h
@@ -14,11 +14,6 @@
 #include <KCModule>
 #include <KCMultiDialog>
 
-class KNoteCollectionConfigWidget;
-class KNotePrintSelectThemeComboBox;
-
-class QLineEdit;
-
 class KNOTES_EXPORT KNoteConfigDialog : public KCMultiDialog
 {
     Q_OBJECT
@@ -28,72 +23,6 @@ public:
 
 public Q_SLOTS:
     void slotOk();
-};
-
-class KNoteDisplayConfig : public KCModule
-{
-    Q_OBJECT
-public:
-    explicit KNoteDisplayConfig(QWidget *parent);
-    /** Reimplemented from KCModule. */
-    void load() override;
-    void save() override;
-};
-
-class KNoteEditorConfig : public KCModule
-{
-    Q_OBJECT
-public:
-    explicit KNoteEditorConfig(QWidget *parent);
-    /** Reimplemented from KCModule. */
-    void load() override;
-    void save() override;
-};
-
-class KNoteMiscConfig : public KCModule
-{
-    Q_OBJECT
-public:
-    explicit KNoteMiscConfig(QWidget *parent);
-    /** Reimplemented from KCModule. */
-    void load() override;
-    void save() override;
-    void defaults() override;
-private Q_SLOTS:
-    void slotHelpLinkClicked(const QString &);
-private:
-    QLineEdit *mDefaultTitle = nullptr;
-};
-
-class KNotePrintConfig : public KCModule
-{
-    Q_OBJECT
-public:
-    explicit KNotePrintConfig(QWidget *parent);
-    /** Reimplemented from KCModule. */
-    void load() override;
-    void save() override;
-    void defaults() override;
-
-private Q_SLOTS:
-    void slotThemeChanged();
-    void slotDownloadNewThemes();
-
-private:
-    KNotePrintSelectThemeComboBox *mSelectTheme = nullptr;
-};
-
-class KNoteCollectionConfig : public KCModule
-{
-    Q_OBJECT
-public:
-    explicit KNoteCollectionConfig(QWidget *parent);
-    /** Reimplemented from KCModule. */
-    void load() override;
-    void save() override;
-
-private:
-    KNoteCollectionConfigWidget *mCollectionConfigWidget = nullptr;
 };
 
 #endif
