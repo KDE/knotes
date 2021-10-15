c5d4551c48d26fc86d941d123c67e78bea6cede4
Laurent Montel <montel@kde.org>
2021-10-15 06:53:27 +0200

    GIT_SILENT: remove old kconf_update

--- src/kconf_update/knotes-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/knotes.desktop/\/org.kde.knotes.desktop/" $kickoffrcname
-fi
