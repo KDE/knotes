2bbab1a38888d90d9e95f495c1aa5df94a24e57b
Laurent Montel <montel@kde.org>
2021-12-12 14:05:19 +0100

    GIT_SILENT: fix some reuse lint warning

--- noteshared/src/Messages.sh
+++ noteshared/src/Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC settings/*.kcfg >> rc.cpp
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp'` -o $podir/libnoteshared.pot
 rm -f rc.cpp
--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC data/*.rc settings/*.kcfg settings/*.kcfg.cmake >> rc.cpp
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp'| grep -v '/tests/'` -o $podir/knotes.pot
 rm -f rc.cpp
