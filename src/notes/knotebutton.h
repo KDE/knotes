436f82506ef092fc34330a7e0b9af241ae6a31f1
Laurent Montel <montel@kde.org>
2021-12-22 20:50:38 +0100

    fix enterEvent qt6 signature

--- src/notes/knotebutton.h
+++ src/notes/knotebutton.h
@@ -21,7 +21,11 @@ public:
     Q_REQUIRED_RESULT QSize sizeHint() const override;
 
 protected:
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    void enterEvent(QEnterEvent *) override;
+#else
     void enterEvent(QEvent *) override;
+#endif
     void leaveEvent(QEvent *) override;
 
     void paintEvent(QPaintEvent *e) override;
